46ef8db146706819256d045a94f93b9375df220f
ESTEVAN TANGARIFE CORREA <estevantangarife259682@correo.itm.edu.co>
2022-04-12 21:24:38 -0500

    improve countries CRUD

---
+++ TiendaOnlineFinal.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TiendaOnlineFinal.Web.Models;
+
+namespace TiendaOnlineFinal.Web.Data
+{
+    public class ApplicationDbContext:DbContext {
+
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Country> Contries{ get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>()
+            .HasIndex(t => t.Name)
+            .IsUnique();
+        
+        
+        }
+
+
+
+
+
+    }
+
+
+
+    }
---
+++ TiendaOnlineFinal.Web/Migrations/20220407020525_Inicial.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TiendaOnlineFinal.Web.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "contries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_contries", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_contries_Name",
+                table: "contries",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "contries");
+        }
+    }
+}
